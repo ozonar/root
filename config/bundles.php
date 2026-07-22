398662e008b48a444a9117cc3929e455b4cf6835
ozonar <ozon@r>
2026-07-23 01:24:09 +0700

    feat: monolog

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
